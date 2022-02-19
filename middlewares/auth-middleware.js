7c99c1ceddf4ec8847e2b3ce86696a0e1970d778
donghyeon23 <gjbmt123@gmail.com>
2022-02-19 15:06:59 +0900

    Fix: conflict

--- middlewares/auth-middleware.js
+++ middlewares/auth-middleware.js
@@ -1,22 +1,13 @@
 const jwt = require('jsonwebtoken')
-<<<<<<< HEAD
-const { User } = require("../schemas/user");
-=======
 const { User } = require('../schemas/user')
->>>>>>> a796cfac6de7c94cb7e086d1463bec59646f1066
 
 module.exports = (req, res, next) => {
     const { authorization } = req.headers
     const [tokenType, tokenValue] = authorization.split(' ')
 
     if (tokenType !== 'Bearer') {
-<<<<<<< HEAD
-        return res.status(401).send({
-            errorMessage: '로그인 후 사용하세요.'
-=======
         res.status(401).send({
             errorMessage: '로그인 후 사용하세요.',
->>>>>>> a796cfac6de7c94cb7e086d1463bec59646f1066
         })
     }
 
