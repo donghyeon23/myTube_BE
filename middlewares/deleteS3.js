126c9e89ce6b4dc4f03bcf187d8803f1e33bcb01
donghyeon23 <gjbmt123@gmail.com>
2022-02-20 12:41:04 +0900

    :bug: Fix s3 delete module
    s3 경로 부분 수정

--- middlewares/deleteS3.js
+++ middlewares/deleteS3.js
@@ -1,11 +1,11 @@
-const AWS = require("aws-sdk");
+const AWS = require('aws-sdk');
 const { S3_ACCESS_KEY_ID, S3_SECRET_ACCESS_KEY, S3_REGION, BUCKET_NAME } = process.env;
 
 module.exports = (article) => {
-    const uri = article.imageUrl.split("/").slice(-1)
-    const uri2 = article.videoUrl.split("/").slice(-1)
-    const key = "original/" + decodeURI(uri);
-    const key2 = "original/" + decodeURI(uri2);
+    const uri = article.imageUrl.split('/').slice(-1);
+    const uri2 = article.videoUrl.split('/').slice(-1);
+    const key = 'images/' + decodeURI(uri);
+    const key2 = 'videos/' + decodeURI(uri2);
 
     const S3 = new AWS.S3({
         accessKeyId: S3_ACCESS_KEY_ID,
@@ -13,17 +13,16 @@ module.exports = (article) => {
         region: S3_REGION,
     });
 
-
     const params = {
         Bucket: BUCKET_NAME,
         Delete: {
             Objects: [{ Key: key }, { Key: key2 }],
-            Quiet: false
-        }
+            Quiet: false,
+        },
     };
     S3.deleteObjects(params, (err, data) => {
-        if (err) console.log(err, err.stack); // an error occurred
+        if (err) console.log(err, err.stack);
+        // an error occurred
         else console.log(data);
     });
-
-}
+};
